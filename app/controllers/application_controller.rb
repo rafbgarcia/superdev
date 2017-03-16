20152f40f5073f27aaea38d935fca1c6ed841533
Rafael Garcia <rafbgarcia@gmail.com>
2017-03-16 13:32:36 -0300

    acme challenge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   after_action :set_csrf_cookie_for_angular_xhr_requests
 
+  def acme_challenge
+    render text: 'vj7p7rywZb1-E1wC8Ug4Eqe7y6wp-TAPCYOV7AyJVCg.BALPitwZqtD4Fya9BP5w6jnmDZlP6S0iYEuXkZjoY9k'
+  end
+
 protected
 
   def set_csrf_cookie_for_angular_xhr_requests
