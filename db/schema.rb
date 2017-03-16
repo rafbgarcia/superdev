b745dc6208081c3b5b05ce697894882e37eb1ccd
Rafael Garcia <rafbgarcia@gmail.com>
2017-03-16 19:54:56 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316172300) do
+ActiveRecord::Schema.define(version: 20170315225354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,8 +147,8 @@ ActiveRecord::Schema.define(version: 20170316172300) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                        default: "",    null: false
-    t.string   "encrypted_password",           default: "",    null: false
+    t.string   "email",                        default: "", null: false
+    t.string   "encrypted_password",           default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -163,12 +163,11 @@ ActiveRecord::Schema.define(version: 20170316172300) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.datetime "created_at",                                   null: false
-    t.datetime "updated_at",                                   null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.string   "iugu_token"
     t.string   "iugu_customer_id"
     t.string   "iugu_subscription_id"
-    t.boolean  "has_active_subscription",      default: false
     t.date     "iugu_subscription_expires_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
