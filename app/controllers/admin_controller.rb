59a47178c89487b5e08147e32849dff8261beb00
Rafael Garcia <rafbgarcia@gmail.com>
2017-03-19 19:48:37 -0300

    admin basic authg

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,8 @@
 class AdminController < ApplicationController
   ActionController::Parameters.permit_all_parameters = true
 
+  http_basic_authenticate_with name: "rafaloco", password: "el secret es la muerte!123."
+
   def index
   end
 
