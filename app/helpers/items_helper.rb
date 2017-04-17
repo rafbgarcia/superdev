9dafbf7613b056e35c8970264681553fee266624
Rafael Garcia <rafbgarcia@gmail.com>
2017-04-17 19:23:08 -0300

    icon for exercises

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -4,6 +4,14 @@ module ItemsHelper
     type.underscore.downcase
   end
 
+  def item_icon(item)
+    if item.video? && item.itemable.url.blank?
+      "question_type_choice_question.png"
+    else
+      "question_type_#{format_item_type(item.type)}.png"
+    end
+  end
+
   def item_path(item)
     course_item_path(
       course_id: item.lesson.course.slug,
